6e50efe7616f92630564cc35ef3faaeeb21227de
Harsh Vardhan Vashistha <hvvashistha@gmail.com>
2016-11-23 11:12:08 -0500

    Adding, VL1 selector language and rule_estimation module, not working yet

--- eleusis_test.py
+++ eleusis_test.py
@@ -54,48 +54,206 @@ functions = {
     'modifier': ['plus1', 'minus1']
     }
 
-def colored(string, color):
-    RED = '\033[91m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    BLUE = '\033[94m'
-    ENDC = '\033[0m'
-    return eval(color.upper()) + str(string) + ENDC
+# START ###############################################################################################################################################
+
+#Language: Extended Propositional
+class Language:
+    def __init__(self):
+        self.complex = []
+
+    def addComplex(self, complexS):
+        self.complex.append(complexS)
+
+    def dnf(self):
+        return self.complex
+
+    def updateDnf(self, dnf):
+        self.complex = dnf
+
+Language.ATTRIBUTE = "ATTRIBUTE"
+Language.CONSTANT = "CONSTANT"
+
+# VL1 representation
+# Class Entity represents an entity, an entity records the attributes an object of that entity is can have and their respective domains
+class Selector:
+    def __init__(self, variableIdentity, variableType, variableDomain, relation, reference, spID = ''):
+        # relation : '==', '>', '>=', '<', '<=', '!='
+        self.variable = (variableIdentity, variableType, variableDomain)
+        self.specialIdentifier = spID
+        self.relation = relation
+        self.reference = reference
+
+    def __str__(self):
+        ref = ''
+        if self.variable[1] == Selector.NOMINAL:
+            ref = ref.join(self.reference)
+        elif isinstance(self.reference, list):
+            ref = ''
+            for i in self.reference:
+                ref = ref + (str(i) if not isinstance(i, tuple) else (str(i[0]) + ".." + str(i[1]))) + ","
+            ref = ref[:-1]
+        else:
+            ref = str(self.reference)
+
+        return "[" + str(self.variable[0] + self.specialIdentifier) + " " + self.relation + " {" + str(ref) + "}]"
+
+    def __cmp__(self, sel):
+        if self.variable == sel.variable:
+            return cmp(self.getExpandedReference(), sel.getExpandedReference())
+        return False
+
+    def getIdentifier(self):
+        return self.variable[0]
+
+    def getName(self):
+        return self.variable[0] + self.specialIdentifier
+
+    def getReference(self):
+        return self.reference
+
+    def getExpandedReference(self):
+        ref = self.getReference()
+        if isinstance(ref, list):
+            fullRef = []
+            for i in ref:
+                if isinstance(i, tuple):
+                    fullRef = fullRef + [j for j in range(i[0], i[1]+1)]
+                else:
+                    fullRef.append(i)
+            return fullRef
+        return ref
+
+    def getDomain(self):
+        return self.variable[2]
+
+    def getExpandedDomain(self):
+        if self.variable[1] == Selector.INTERVAL and isinstance(self.variable[2], tuple):
+            return [i for i in range(self.getDomain()[0], self.getDomain()[1] + 1)]
+        return self.getDomain()
+
+    def getType(self):
+        return self.variable[1]
+
+    def setSPID(self, spID = ''):
+        self.specialIdentifier = str(spID)
+
+    def setReferece(self, reference):
+        self.reference = reference
+
+    def setRelation(self, rel):
+        self.relation = rel
+
+    def test(self, value):
+        if isinstance(value, Selector):
+            value = value.getReference()
+        ref = self.getExpandedReference()
+        if isinstance(ref, list):
+            return value in ref
+        return eval("'" + str(value) + "'" + self.relation + "'" + str(ref) + "'")
+
+    def difference(self, sel):
+        assert self.variable == sel.variable
+        name = ('d' + self.variable[0] + self.specialIdentifier + sel.specialIdentifier)
+        if self.variable[1] == Selector.NOMINAL:
+            return Selector(name, Selector.INTERVAL, [0, 1], '==',
+                1 if self.reference != sel.reference else 0)
+        elif self.variable[1] == Selector.CIRCULAR:
+            dLimit = len(self.variable[2]) - 1
+            return Selector(name, Selector.INTERVAL, [(-dLimit, dLimit)], '==',
+                self.variable[2].index(self.reference) - sel.variable[2].index(sel.reference))
+            # return len(self.variable[2]) % (self.variable[2].index(self.reference) - sel.variable[2].index(sel.reference))
+        else:
+            dLimit = self.variable[2][0][1] - self.variable[2][0][0]
+            return Selector(name, Selector.INTERVAL, [(-dLimit, dLimit)], '==',
+                self.reference - sel.reference)
+
+
+
+#Attribute Types
+Selector.NOMINAL = "NOMINAL"
+Selector.CIRCULAR = "CIRCULAR"
+Selector.INTERVAL = "INTERVAL"
+
+def setToRange(rangeSet):
+    r = []
+    for i in rangeSet:
+        if isinstance(i, tuple):
+            r = r + range(i[0], i[1]+1)
+        else:
+            r.append(i)
+    return r
+
+class Operator:
+    def __init__(self, operator, paramSize = 1):
+        if type(operator) == type(self.noop):
+            self.op = operator
+        else:
+            operator = self.noop
+
+        self.paramSize = paramSize
+
+    def test(self, *args):
+        return self.op(*args)
+
+    def noop(self):
+        return None
+
+class Formula:
+    def __init__(self, operator, entity, attributes, attributeTypes):
+        assert isinstance(operator, Operator)
+        assert isinstance(attributes, list)
+        assert len(attributes) == operator.paramSize
+        assert isinstance(attributeTypes, list)
+        assert len(attributeTypes) == operator.paramSize
+        self.operator = operator
+        self.attributes = attributes
+        self.types = attributeTypes
+
+# END #################################################################################################################################################
 
 class Card:
     def __init__(self, value, suit):
-        if isinstance(value, int):
-            value = eleusis.value_to_number(value)
+        if not isinstance(value, int):
+            value = int(eleusis.value_to_number(value))
 
-        if not value in Card.values or not suit in Card.suit:
-            print 'Wrong Values'
-            raise Exception('Wrong Values')
-
-        self.card = value + suit
+        # if not Card.definition.inDomain("value", value) or not Card.definition.inDomain("suit", suit):
+        #     print 'Wrong Values'
+        #     raise Exception('Wrong Values')
+        self.value = Selector("value", Selector.INTERVAL, Card.domain["value"], '==', value)
+        self.suit = Selector("suit", Selector.CIRCULAR, Card.domain["suit"], '==', suit)
+        self.color = Selector("color", Selector.NOMINAL, Card.domain["color"], '==', Card.color[suit])
 
     def __str__(self):
-        return self.card
+        return str(self.value.getReference()) + str(self.suit.getReference())
 
     def __cmp__(self, other):
         if isinstance(other, Card):
-            return cmp(self.card, other.card)
+            return self.value.getReference() == other.value.getReference() and self.suit.getReference() == other.suit.getReference()
         return False
 
-Card.values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-Card.suit = ['C', 'D', 'H', 'S']
-Card.suitColors = {
-    'C' : 'B',
-    'D' : 'R',
-    'H' : 'R',
-    'S' : 'B'
+    def __iter__(self):
+        yield self.value
+        yield self.suit
+        yield self.color
+
+Card.domain = {
+    "value": [(1,13)],
+    "suit": ['C', 'D', 'H', 'S'],
+    "color": ['B', 'R']
+}
+Card.color = {
+    'C': 'B',
+    'D': 'R',
+    'H': 'R',
+    'S': 'B'
 }
 
 class Deck:
     def __init__(self, deckMultiples = 1):
         self.deck = []
         deck = []
-        for suit in Card.suit:
-            for value in Card.values:
+        for suit in Card.domain["suit"]:
+            for value in setToRange(Card.domain["value"]):
                 deck.append(Card(value, suit))
 
         for i in range(0, deckMultiples):
@@ -147,6 +305,14 @@ class Player:
             print str(card) + ", ",
         sys.stdout.write('\010\010\n')
 
+def colored(string, color):
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    ENDC = '\033[0m'
+    return eval(color.upper()) + str(string) + ENDC
+
 class Game:
     def __init__(self, godPlay, players = 6, randomPlay = False, rule = None):
         self.deck = Deck(6)
@@ -157,10 +323,11 @@ class Game:
         self.playCounter = -1
         self.randomPlay = randomPlay
         self.ruleTree = None
+        self.playRun = 0
         if rule is not None:
             self.ruleTree = eleusis.parse(rule)
 
-        self.recordPlay([str(godPlay)], True)
+        self.recordPlay([godPlay], True)
 
         for i in range(0, players):
             player = Player()
@@ -174,6 +341,7 @@ class Game:
     def callPlay(self, player, correct = True):
         if not correct:
             cards = self.deck.drawCards(5)
+
             if cards is None or len(cards) < 5:
                 return None
             player.addCards(cards)
@@ -186,12 +354,12 @@ class Game:
         if len(self.history) > 0:
             for f in functions['attribute']:
                 try:
-                    new_record.append(f(lastPlays[2]))
+                    new_record.append(f(str(lastPlays[2])))
                 except:
                     try:
-                        new_record.append(f(lastPlays[2],lastPlays[1]))
+                        new_record.append(f(str(lastPlays[2]),str(lastPlays[1])))
                     except:
-                        new_record.append(f(lastPlays[2],lastPlays[1], lastPlays[0]))
+                        new_record.append(f(str(lastPlays[2]),str(lastPlays[1]), str(lastPlays[0])))
 
             self.truthTable.append(new_record)
         self.history.append((lastPlays[-1], correct))
@@ -213,13 +381,14 @@ class Game:
         sys.stdout.write('\010\010\n\n')
 
 
-    def playNext(self):
+    def playNext(self, runCount = None):
         player = self.nextPlayer()
         play = None
         card = None
         os.system('clear')
         self.printRecord()
         lastPlays = []
+        events = []
         if self.randomPlay:
             play = player.playRandom()
         else:
@@ -244,25 +413,34 @@ class Game:
 
             for i in range(historyLen, -1, -1):
                 if self.history[i][1]:
+                    events.insert(0, copy.deepcopy(self.history[i][0]))
                     lastPlays.insert(0, str(self.history[i][0]))
                 if len(lastPlays) >= 2:
                     break
+            events.append(copy.deepcopy(play))
             lastPlays.append(str(play))
             while len(lastPlays) < 3:
+                events.insert(0, None)
                 lastPlays.insert(0, None)
-
-            # pdb.set_trace()
 
             correct = self.ruleTree.evaluate(lastPlays)
 
-        if play is None or self.callPlay(player, correct) is None:
-            return "Game Stops"
+        nextPlay = []
 
-        self.recordPlay(lastPlays, correct)
+        if play is None or self.callPlay(player, correct) is None:
+            return nextPlay
+
+        self.recordPlay(events, correct)
         # raw_input()
         os.system('clear')
         self.printRecord()
-        self.playNext()
+        if self.randomPlay and (runCount is None or self.playRun < runCount):
+            self.playRun = self.playRun + 1
+            nextPlay = self.playNext(runCount = runCount)
+        else:
+            self.playRun = 0
+
+        return [(events, correct)] + nextPlay
 
 if __name__ == "__main__":
     game = Game(Card(sys.argv[1][:-1], sys.argv[1][len(sys.argv[1])-1:]), rule=sys.argv[2], randomPlay = True)
---
+++ rule_estimation.py
@@ -0,0 +1,235 @@
+import sys
+import pdb
+import eleusis
+import eleusis_test as GameModule
+from eleusis_test import Selector
+
+lang = GameModule.Language()
+
+def notF(inputV):
+    return not inputV
+
+def evenF(inputV):
+    return inputV % 2 == 0
+
+def distanceF(inputV1, inputV2, domain):
+    l = None
+    if isinstance(domain, set):
+        return inputV2 - inputV1
+    else:
+        return domain.index(inputV2) - domain.index(inputV1)
+
+
+operators = {
+    'notO': GameModule.Operator(notF, 1),
+    'evenO': GameModule.Operator(evenF, 1),
+    'greaterO': GameModule.Operator(eleusis.to_function['greater'], 2),
+    'lessO': GameModule.Operator(eleusis.to_function['less'], 2),
+    'equalO': GameModule.Operator(eleusis.to_function['equal'], 2),
+    'distanceO': GameModule.Operator(distanceF, 3)
+}
+
+params = {
+    'lookback': 1
+}
+
+def printDesc(event):
+    for desc in event:
+        print str(desc if isinstance(event, list) else event[desc]),
+    print ''
+
+## Problem specific, Given two episode descriptions, generate meta attributes between them
+def getMetaAttributes(episode1, episode2):
+    #dvalue, dsuit, dcolor
+    metaAttributeList = ['value', 'suit', 'color']
+
+    metaAttributes = {}
+
+    for i in metaAttributeList:
+        metaAttributes['d' + i + str(episode1[1]) + str(episode2[1])] = episode1[0][i + str(episode1[1])].difference(episode2[0][i + str(episode2[1])])
+    return metaAttributes
+
+## Problem specific, breaks down Entities like Card into their attributes
+def getEventEpisodeDescription(eventEpisode, lookback = None):
+    description = {}
+    for sel in eventEpisode:
+        sel.setSPID(lookback)
+        description[sel.getName()] = sel
+
+    return description
+
+
+##########################
+# Learning system
+
+# Generates a full description of events in each sequence, adding meta attributes and separating them into positive and negative sets
+def describe(events, lookback):
+    pos = []
+    neg = []
+    for event in events:
+        eventDescription = {}
+        size = len(event[0]) - 1
+        outcome = getEventEpisodeDescription(event[0][size], 0)
+        eventDescription.update(outcome)
+        for i in range(0, lookback):
+            index = size - lookback
+            lookbackDesc = getEventEpisodeDescription(event[0][index], i + 1)
+            lookbackDesc.update(getMetaAttributes((outcome, 0), (lookbackDesc, i+1)))
+            eventDescription.update(lookbackDesc)
+
+        # printDesc(eventDescription)
+
+        if event[1]:
+            pos.append(eventDescription)
+        else:
+            neg.append(eventDescription)
+    return pos, neg
+
+#remove from reference
+def getComplimentaryReference(type, originalcover, removeCover=[], addCover=[]):
+    newCover = []
+
+    if isinstance(removeCover, tuple):
+        removeCover = range(removeCover[0], removeCover[1] + 1)
+    elif not isinstance(removeCover, list):
+        removeCover = [removeCover]
+
+    if isinstance(addCover, tuple):
+        addCover = range(addCover[0], addCover[1] + 1)
+    elif not isinstance(addCover, list):
+        addCover = [addCover]
+
+    for i in originalcover:
+        if isinstance(i, tuple):
+            newCover = newCover + [j for j in range(i[0], i[1]+1) if j not in removeCover]
+        elif i not in removeCover:
+            newCover.append(i)
+
+    newCover = list(set(newCover) | set(addCover))
+    newCover.sort()
+
+    if type == Selector.INTERVAL:
+        originalcover = newCover
+        newCover = []
+        index = 0
+        counter = 0
+        for i in range(1, len(originalcover)):
+
+            if originalcover[i] - originalcover[i-1] == 1:
+                counter = counter + 1
+
+            if originalcover[i] - originalcover[i-1] > 1:
+                if counter < 2:
+                    newCover = newCover + originalcover[index: index+counter + 1]
+                else:
+                    newCover.append((originalcover[index], originalcover[index+counter]))
+                index = i
+                counter = 0
+        if counter < 2:
+            newCover = newCover + originalcover[index: index+counter + 1]
+        else:
+            newCover.append((originalcover[index], originalcover[index+counter]))
+
+    return newCover if len(newCover) > 1 else newCover[0]
+
+def Star(seed, negatives):
+    r = []
+    # print "seed: ",
+    # printDesc(seed)
+
+    star = []  #Covers everything
+    ruleCover = []  #Useless, when star above is implemented
+
+    for neg in negatives:
+        # print "negative: ",
+        # printDesc(neg)
+        ruleComplex = []
+        # Generate complementary complex for negative event
+        for sel in neg:
+            selector = neg[sel]
+
+            if seed[selector.getName()] != selector:
+                #Above condition is equivalent to LEF trimming on maxstar, #Compliment of negative event covers seed only when neg != pos
+
+                #Getting compliment of negative, covers positive
+                selector.setReferece(getComplimentaryReference(selector.getType(), selector.getDomain(), removeCover=selector.getReference()))
+
+                # if selector.test(seed[selector.getName()]):
+                # intersection = star[selector.getName()].intersect(selector)
+                # if intersection.test(seed[intersection.getName()]):
+                #     star[intersection.getName()] = intersection
+
+                ruleComplex.append(selector)    #useless, directly intersect
+
+        ruleCover.append(ruleComplex)
+
+    print "cover: "
+    for i in ruleCover:
+        printDesc(i)
+
+## Generate a cover of all positives, so that no negative is covered
+def AQ(pos, neg):
+    for seed in pos:
+        ruleSet = Star(seed, neg)
+        pdb.set_trace()
+
+##################
+
+def emulate_Play(numberOfPlayers = 6):
+    rule = sys.argv[2]
+    numberOfPlays = 1
+    game = GameModule.Game(GameModule.Card(sys.argv[1][:-1], sys.argv[1][len(sys.argv[1])-1:]), rule=sys.argv[2], randomPlay = True, players=numberOfPlayers)
+    #God played
+
+    historicalSequences = []
+
+    eventSets = {
+        'positive': [],
+        'negative': []
+    }
+
+    #Time for players to play
+    for i in range(0, numberOfPlays):
+        pos, neg = describe(game.playNext(runCount = numberOfPlayers), params['lookback'])
+        eventSets['positive'] = eventSets['positive'] + pos
+        eventSets['negative'] = eventSets['negative'] + neg
+        if len(pos) == 0 or len(neg) == 0:
+            continue
+        else:   #Start learning here
+
+            # Decomposition model fitting
+            # decompose(eventSets['positive'], eventSets['negative'])
+
+            # Periodic model fitting, if required
+            #
+
+            #DNF Model fitting, Very large search space, execute only if necessary
+            AQ(eventSets['positive'], eventSets['negative'])
+
+
+                #Learning code ends
+            #Reset event set, for new learning episode
+            historicalSequences.append(eventSets)
+            eventSets['positive'] = []
+            eventSets['negative'] = []
+
+if __name__ == "__main__":
+    emulate_Play()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
