9842f6d7554fbfd099f1866322a932e417ff0be7
Sabin Raj Tiwari <sabin.tiwari@gmail.com>
2016-11-22 19:30:05 -0500

    A few more additions to scientist.py

--- scientist.py
+++ scientist.py
@@ -1,4 +1,15 @@
+import random
 import eleusis
+
+# globals
+card_values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+card_suits = ['C', 'D', 'H', 'S']
+card_colors = {
+    'C' : 'B',
+    'D' : 'R',
+    'H' : 'R',
+    'S' : 'B'
+}
 
 class Scientist:
 
@@ -6,37 +17,52 @@ class Scientist:
 		self.dealer_rule = None
 		self.rules = []
 		self.board = []
-
-	def start(self):
-		counter = 0
-		
+		self.total_moves = 0
+		self.all_cards = []
+		for card in cards:
+			self.board.append((card, []))
+		self.set_rule(rule)
+		self.build_all_cards()
 
 	#region Main Functions
 
 	#	This function will set the dealer rule using the
-	#	helper functions in new_eleusis.py
+	#	helper functions in new_eleusis.py.
+	#	TODO: Need to update a few things based on prof's suggestions.
 	def set_rule(self, expression):
-		self.dealer_rule = eleusis.tree(expression)
+		self.dealer_rule = eleusis.parse(expression)
 
 	# 	This function will return the current actual rule that the player is
-	#	considering and will be used to determine the score
+	#	considering and will be used to determine the score.
+	#	Returs the best rule in the list of rules
 	def rule(self):
 		if len(self.rules) > 0:
-			return self.prioritize_rules(rules)[0]
+			return self.prioritize_rules(self.rules)[0]
 		return None
 	
 	#	This function will display the board state in string representation
+	#	Returns string for the board
 	def board_state(self):
-		board = "{"
+		board = "{\n"
 		for card in self.board:
-			board = board + "(" + card[0] + ", " + str(card[1]) + ")"
-			board = board + "}"
+			board = board + "(" + card[0] + ", " + str(card[1]) + ")\n"
+		board = board.strip(" ").strip(",")
+		board = board + "}"
 		return board
 
-	#	This function will play a card and determine if it satisfies the rule
-	#	This also updates the board_state
+	#	This function will play a card and determine if it satisfies the rule.
+	#	This also updates the board_state.
+	#	Returns true or false
 	def play(self, card):
-		if self.dealer_rule.evaluate(card):
+		self.total_moves = self.total_moves + 1
+		last_three = []
+		last_three.append(self.board[-1][0])
+		if len(self.board) < 2:
+			last_three.insert(0, None)
+		else:
+			last_three.insert(0, self.board[-2][0])
+		last_three.append(card)
+		if self.dealer_rule.evaluate(last_three):
 			self.board.append((card, []))
 			return True
 		else:
@@ -44,20 +70,28 @@ class Scientist:
 			return False
 
 	#	This function handles the player which will determine when and what to play
-	#	and choosing when to declare success
-	def scientist(self, counter):
+	#	and choosing when to declare success.
+	def scientist(self):
 		end = False
+		# first move play at random
+		self.play(self.get_random_card())
+		print self.board_state()
 		while not end:
-			print "HELLO"
-			end = True
+			raw_input('Press enter to continue.')
+			self.rules = self.generate_rules()
+			self.play(self.get_best_card())
+			print self.board_state()
+			self.decide_success()
+		print "Finished."
 
-	#	Returns the score of the game
+	#	Adds 1 for every right card, 2 for every wrong card, 15 for a wrong rule, 
+	#		and 30 for a rule that does not satisfy the cards on the board.
+	#	Returns a value for the score
 	def score(self):
 		score = 0
 		current_rule = self.rule()
 		for card in self.board:
-			score = score + 1
-			score = score + (2 * len(card[1]))
+			score = score + 1 + (2 * len(card[1]))
 		if current_rule != self.dealer_rule:
 			score = score + 15
 		if not current_rule.evaluate(board):
@@ -68,16 +102,41 @@ class Scientist:
 
 
 	#region Helper Functions
+
+	#	Based on the current possible rules, decides the best card to play.
+	#	Applies constrainsts at this stage.
+	#	Returns the card to play.
+	def get_best_card(self):
+		previous2 = None
+		previous = None
+		current = self.board[-1]
+		if len(self.board > 2):
+			previous2 = self.board[-3]
+		elif len(self.board > 1):
+			previous = self.board[-2]
+		
+		
+		#return self.get_random_card()
 	
+	#	Generates rules that apply to the current state of the board. 
+	#	Only a decent amount will be chosen sicne there could be infinute possible rules.
+	#	Applies constraints at this stage.
+	# 	ODO: Infer Rules
+	def generate_rules(self):
+		correct_rules = []
+		return correct_rules
+
+
 	#	Sets priority to all the rules that are being considered currently.
 	#	Uses a list of rules that contain the attribute: evaluation
 	#	Returns the sorted list of rules.
-	def prioritize_rules(self):
+	def prioritize_rules(self, rules):
 		for rule in rules:
 			rule.evaluation = self.evaluate_rule(rule)
 		return sort(rules)
 
 	#	Evaluates a rule based on how many correct and wrong cards it produced
+	#	Returns the value for the evaluation (0 - 100)
 	def evaluate_rule(self, rule):
 		total = 0;
 		correct = 0;
@@ -88,10 +147,29 @@ class Scientist:
 		return (correct / total) * 100
 
 	#	Calculates if the player should decide success
+	#	Returns true or false depending on the player needing to decide
 	def decide_success(self):
 		current_rule = self.rule()
-		if len(self.board) > 20 and current_rule.evaluate(board):
-			if rule.evaluation == 100 or len(self.board) > 200:
-				return True
-			return False
+		if not current_rule is None:
+			if len(self.board) > 20 and current_rule.evaluate(board):
+				if rule.evaluation == 100 or self.total_moves > 200:
+					return True
 		return False
+
+	#	Builds the list of all possible cards
+	def build_all_cards(self):
+		for value in card_values:
+			for suit in card_suits:
+				self.all_cards.append(value + suit)
+
+	#	Gets a random card to play from the whole deck.
+	#	Returns a card.
+	def get_random_card(self):
+		return self.all_cards[int(round(random.random() * (len(self.all_cards) - 1)))]
+
+	#end Helper Functions
+
+
+if __name__ == "__main__":
+    sci = Scientist(["4H"], "even(current)")
+    sci.scientist()
